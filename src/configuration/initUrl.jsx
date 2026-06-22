6ceb96846a9791ed4ce7f4a230920dd8b74aaab0
Ashna Naushad <Ashnanaushad2005@outlook.com>
2026-06-22 11:41:43 +0530

    Updated API configuration and gitignore

--- src/configuration/initUrl.jsx
+++ src/configuration/initUrl.jsx
@@ -4,3 +4,6 @@ export const ORG_NAME = "Remindar"
 //Local
  export const BASE_URL = "https://localhost:7161/"
  export const BASE_URL_API = "https://localhost:7161/api"
+
+// export const BASE_URL = "http://localhost:5175/"
+// export const BASE_URL_API = "http://localhost:5175/api"
